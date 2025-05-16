450a145ace328b007e7ce83a1817a3ae38c265a1
AoTo <68062035+AoTo0330@users.noreply.github.com>
2025-05-16 14:24:27 +0900

    Create main.tf
    add main.tf draft for iac team

---
+++ infra/main.tf
@@ -0,0 +1,32 @@
+variable "gcp_project_id" {
+  type = string
+}
+variable "primary_region" {
+  type = string
+}
+
+(* terraform {
+  backend "local" {
+    path = ".tfstate/<state_file_name>"
+  }
+} *)
+
+## project ##
+provider "google" {
+  project = var.gcp_project_id
+  region  = var.primary_region
+}
+
+## Cloud Run ##
+module "cloud-run" {
+  source         = "./modules/cloud-run"
+  gcp_project_id = var.gcp_project_id
+}
+
+
+## Artifact Registry ##
+module "artifact-registry" {
+  source                     = "./modules/artifact-registry"
+  gcp_project_id             = var.gcp_project_id
+  artifact_registry_location = var.primary_region
+}
